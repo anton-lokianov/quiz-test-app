3f5db407335d2d82050f695094e257b0c208adcf
anton lokianov <antonlokianov@gmail.com>
2024-04-15 20:10:08 +0300

    test

---
+++ script/index.js
@@ -0,0 +1,110 @@
+import { quizQuestions } from "./quiz.js";
+
+let currentLevel = 1;
+const mainContainer = document.querySelector("main");
+const quizContainer = document.createElement("div");
+
+const startQuizBtn = document.querySelector("#startQuiz");
+startQuizBtn.addEventListener("click", () => {
+  startQuizBtn.classList.add("hidden");
+  renderQuestion();
+});
+
+const getRandQuestion = (level) => {
+  const questions = quizQuestions[`level${level}`];
+  const randomQuestion = Math.floor(Math.random() * questions.length);
+  return questions[randomQuestion];
+};
+
+const renderQuestion = () => {
+  if (quizContainer.classList.contains("animate__bounceOutDown")) {
+    quizContainer.classList.remove("animate__bounceOutDown");
+  }
+  quizContainer.className =
+    "quiz-container animate__animated animate__bounceInDown";
+  quizContainer.innerHTML = "";
+
+  const options = document.createElement("div");
+  options.classList.add("options");
+
+  const currentQuestion = getRandQuestion(currentLevel);
+
+  const questionText = document.createElement("p");
+  questionText.classList.add("question-text");
+  questionText.textContent = currentQuestion.question;
+  quizContainer.appendChild(questionText);
+
+  currentQuestion.options.forEach((option, idx) => {
+    const answerBtn = document.createElement("button");
+    answerBtn.textContent = `${idx + 1}. ${option}`;
+    answerBtn.setAttribute("data-option", option);
+    options.appendChild(answerBtn);
+    answerBtn.addEventListener(
+      "click",
+      checkAnswerHandler(option, currentQuestion)
+    );
+  });
+
+  quizContainer.appendChild(options);
+  mainContainer.appendChild(quizContainer);
+};
+
+const checkAnswerHandler = (selectedOption, currentQuestion) => {
+  return () => {
+    const options = document.querySelectorAll(".options button");
+    options.forEach((option) => {
+      option.disabled = true;
+      option.classList.add("disabled-button");
+
+      if (option.getAttribute("data-option") === selectedOption) {
+        option.classList.add("active");
+
+        if (selectedOption === currentQuestion.answer) {
+          option.classList.add("correct");
+          currentLevel++;
+          if (currentLevel > 3) {
+            setTimeout(() => {
+              finishedQuizVideo();
+            }, 700);
+          } else {
+            setTimeout(() => {
+              ExitAnimation();
+            }, 700);
+          }
+        } else {
+          option.classList.add("wrong");
+          option.classList.add("animate__animated", "animate__headShake");
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        }
+      }
+    });
+  };
+};
+
+const ExitAnimation = () => {
+  quizContainer.classList.replace(
+    "animate__bounceInDown",
+    "animate__bounceOutDown"
+  );
+  quizContainer.addEventListener(
+    "animationend",
+    () => {
+      renderQuestion();
+    },
+    { once: true }
+  );
+};
+
+const finishedQuizVideo = () => {
+  const youtubeVideo = document.createElement("iframe");
+  youtubeVideo.src = "https://www.youtube.com/embed/4gPJQr3Q7fc?autoplay=1";
+  youtubeVideo.allow =
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+  youtubeVideo.className = "finishedQuiz-video";
+  youtubeVideo.frameborder = "0";
+
+  quizContainer.classList.add("hidden");
+  mainContainer.appendChild(youtubeVideo);
+};
---
+++ script/quiz.js
@@ -0,0 +1,58 @@
+export const quizQuestions = {
+  level1: [
+    {
+      question: "What color is the sky on a clear day?",
+      options: ["Red", "Blue", "Green", "Yellow"],
+      answer: "Blue",
+    },
+    {
+      question: "How many letters are there in the English alphabet?",
+      options: ["20", "26", "24", "22"],
+      answer: "26",
+    },
+    {
+      question: "What do bees produce?",
+      options: ["Milk", "Honey", "Wool", "Cotton"],
+      answer: "Honey",
+    },
+  ],
+  level2: [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Rome", "Berlin"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Jupiter", "Venus"],
+      answer: "Mars",
+    },
+    {
+      question: "What gas do plants absorb from the environment?",
+      options: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+      answer: "Carbon Dioxide",
+    },
+  ],
+  level3: [
+    {
+      question: "Who wrote 'Macbeth'?",
+      options: [
+        "William Shakespeare",
+        "Charles Dickens",
+        "Jane Austen",
+        "Leo Tolstoy",
+      ],
+      answer: "William Shakespeare",
+    },
+    {
+      question: "What is the chemical symbol for water?",
+      options: ["H2O", "CO2", "O2", "N2"],
+      answer: "H2O",
+    },
+    {
+      question: "What year did the first manned Moon landing occur?",
+      options: ["1955", "1969", "1972", "1980"],
+      answer: "1969",
+    },
+  ],
+};
